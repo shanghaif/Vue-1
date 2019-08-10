4d72d6ff9ef4e446b5510c936d4db72bf1355e08
Gary <596765672@qq.com>
2019-08-10 22:16:31 +0800

    修改缓存方式

--- vue-康美项目/km-ctms-mobile/src/utils/auth.js
+++ vue-康美项目/km-ctms-mobile/src/utils/auth.js
@@ -1,29 +1,34 @@
 import Cookies from 'js-cookie'
 
-const TokenKey_H5 = 'key_for_token_h5'
-const TokenKey_360App = 'key_for_token_360App'
+const TokenKey_H5 = 'token_H5'
+const TokenKey_360App = 'token_360App'
 
 export function getToken_H5() {
-  return Cookies.get(TokenKey_H5)
+  return localStorage.getItem(TokenKey_H5)
+  // return Cookies.get(TokenKey_H5)
 }
 
 export function setToken_H5(token) {
-  return Cookies.set(TokenKey_H5, token)
+  localStorage.setItem(TokenKey_H5, token)
+  // return Cookies.set(TokenKey_H5, token)
 }
 
 export function removeToken_H5() {
-  return Cookies.remove(TokenKey_H5)
+  localStorage.removeItem(TokenKey_H5)
+  // return Cookies.remove(TokenKey_H5)
 }
 
-
 export function getToken_360App() {
-  return Cookies.get(TokenKey_360App)
+  return localStorage.getItem(TokenKey_360App)
+  // return Cookies.get(TokenKey_360App)
 }
 
 export function setToken_360App(token) {
-  return Cookies.set(TokenKey_360App, token)
+  localStorage.setItem(TokenKey_360App, token)
+  // return Cookies.set(TokenKey_360App, token)
 }
 
 export function removeToken_360App() {
-  return Cookies.remove(TokenKey_360App)
+  localStorage.removeItem(TokenKey_360App)
+  // return Cookies.remove(TokenKey_360App)
 }
